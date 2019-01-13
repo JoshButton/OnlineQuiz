f9f5213f5ed14badf3ed5355f4a00fd2d98f2d68
Joshua Button <34740880+JoshButton@users.noreply.github.com>
2019-01-13 20:42:32 +0000

    Add welcome prompt

--- quiz.js
+++ quiz.js
@@ -12,9 +12,11 @@ var questions = [
 	         answer: "a"
 	    }
 	];
-        function playQuiz(){
+function playQuiz(name){
 	var score = 0;
-
+	
+	window.prompt("Hi " + name + " welcome to my quiz");
+	
 	for(var i = 0; i < questions.length; i++){
 	    var response = window.prompt(questions[i].prompt);
 	    if(response == questions[i].answer){
